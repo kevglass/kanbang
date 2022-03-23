a07e55daee41ea778544facb6697e8acd04a08ba
Kevin Glass <kglass@cafex.com>
2022-03-23 14:30:16 +0000

    Updates for connecting to postgres

--- local.md
+++ local.md
@@ -5,3 +5,9 @@ To run locally.
 3. Build project
 4. Run `heroku config:get DATABASE_URL -a kanbang -s >> .env` to get the database URL (this might need to be updated later)
 5. Run `heroku local`
+
+Postgres:
+
+https://postgresapp.com/downloads.html
+
+https://www.postgresql.org/ftp/pgadmin/pgadmin4/v6.7/macos/
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { Pool, Client } = require('pg')
+
 const PORT = process.env.PORT || 5000
 const DATABASE_URL = process.env.DATABASE_URL;
 
 console.log(PORT);
 console.log(DATABASE_URL);
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: {
+     require: true,
+     rejectUnauthorized: false,
+  }
+})
+
+pool.query('select * from boards', (err, res) => {
+ console.log("Initial database check");
+ if (err) {
+ 	console.log(err, res);
+ } else {
+	console.log("Boards: " + res.rowCount);
+ }
+
+ pool.end();
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
